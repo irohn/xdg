deeffa113e82407f82f164012397a5c50fd944ea
ori <orisneh@gmail.com>
2024-12-10 19:07:13 +0200

    Added sessionizer script

---
+++ config/tmux/sessionizer.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+FIND_DEPTH=10
+FIND_DIR="$HOME"
+
+excludes=(
+    .local
+    .cargo
+    .cache
+    .npm
+    node_modules
+    .virtualenvs
+    venv
+    .venv
+    __pycache__
+    .pytest_cache
+    .mypy_cache
+    .pyenv
+    target
+    dist
+    build
+    out
+)
+
+if command -v "fd" 2>&1 >/dev/null; then
+    find_command="fd -H -t d '^.git$' $FIND_DIR $(printf -- "--exclude %s " "${excludes[@]}") --max-depth ${FIND_DEPTH} -x echo {//}"
+else
+    find_command="find $FIND_DIR -maxdepth ${FIND_DEPTH} -name .git -type d $(printf -- "-not -path '*/%s/*' " "${excludes[@]}") -exec dirname {} \;"
+fi
+
+if command -v "fzf" 2>&1 >/dev/null; then
+    selected_dir=$(bash -c "$find_command | fzf --preview 'ls -la'")
+else
+    read -p "Sessionize Directory: " dir_input
+    selected_dir=$(bash -c "$find_command | grep "$dir_input" | head -1")
+fi
+
+session_name=$(basename "$selected_dir" | tr . _)
+if ! tmux has-session -t="$session_name" 2>/dev/null; then
+    tmux new-session -ds "$session_name" -c "$selected_dir"
+fi
+[ -z "$TMUX" ] && tmux attach -t "$session_name" || tmux switch-client -t "$session_name"
+
