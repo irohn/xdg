931974251ca14caf313f77866cd2b106604df214
ori <orisneh@gmail.com>
2024-12-15 21:27:02 +0200

    fixed keybind to keymaps

--- config/nvim/after/ftplugin/sh.lua
+++ config/nvim/after/ftplugin/sh.lua
@@ -5,11 +5,4 @@ map("n", "<leader>w", "<cmd>w | !$SHELL -c %<cr>", {
   desc = "source current file",
 })
 
-map("n", "<leader>tr", function()
-  vim.cmd.split()
-  vim.cmd.term()
-  vim.api.nvim_win_set_height(0, 5)
-  vim.cmd.startinsert()
-end)
-
 -- vim: ts=2 sts=2 sw=2 et
