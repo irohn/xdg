f63ead16f0ae04e91236cd633aecd3e84286a25a
ori <orisneh@gmail.com>
2024-12-21 03:36:13 +0200

    added keymaps for lsp actions

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -20,6 +20,55 @@ return {
 			},
 		},
 		config = function(_, opts)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(ev)
+					local keymaps = {
+						["gd"] = vim.lsp.buf.definition,
+						["gr"] = vim.lsp.buf.references,
+						["gI"] = vim.lsp.buf.implementation,
+						["gT"] = vim.lsp.buf.type_definition,
+						["gD"] = vim.lsp.buf.declaration,
+						["<leader>rn"] = vim.lsp.buf.rename,
+						["<leader>ca"] = vim.lsp.buf.code_action,
+					}
+
+					for key, map in pairs(keymaps) do
+						vim.keymap.set("n", key, map, { buffer = ev.buf })
+					end
+
+					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocuments_documentHighlight) then
+						local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = ev.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = ev.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+							callback = function(ev2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = ev2.buf })
+							end,
+						})
+
+						if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+							vim.keymap.set("n", "<leader>ih", function()
+								vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = ev.buf }))
+							end)
+						end
+					end
+				end,
+			})
+
 			local lspconfig = require("lspconfig")
 			for server, config in pairs(opts.servers) do
 				config.capabilities = require("blink.cmp").get_lsp_capabilities()
