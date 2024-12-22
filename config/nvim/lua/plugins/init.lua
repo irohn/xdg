4584170cc2e28e9f3a513c541575d4c07a1b665f
ori <orisneh@gmail.com>
2024-12-22 11:34:34 +0200

    added mini surround

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -10,12 +10,10 @@ return {
 		"echasnovski/mini.nvim",
 		version = "*",
 		config = function()
-			-- text alignment plugin
 			require("mini.align").setup()
-			-- icon provider (requires patched font)
 			require("mini.icons").setup()
-			-- auto pair common characters
 			require("mini.pairs").setup()
+			require("mini.surround").setup()
 		end,
 	},
 }
