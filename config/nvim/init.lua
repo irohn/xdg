854423f8f5382a1fb416346ad074b50c6c393f3e
ori <orisneh@gmail.com>
2025-01-12 00:25:44 +0200

    Changed neovim structure

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,3 @@
--- set leader keys
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- load plugin manager
-require("config.lazy")
-
 require("irohn")
 
 -- vim: ts=2 sts=2 sw=2 et
--- config/nvim/lua/irohn/init.lua
+++ config/nvim/lua/irohn/init.lua
@@ -1,3 +1,10 @@
+-- set leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- configure lazy
+require("irohn.lazy")
+
 require("irohn.autocmds")
 require("irohn.keymaps")
 require("irohn.options")
