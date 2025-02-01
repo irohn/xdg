4c4bfa80bebbcaa212b965d7a7aa72f7ffc71851
ori <orisneh@gmail.com>
2025-02-01 20:59:46 +0200

    added trigger keys for copilot suggestions

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,19 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    opts = {},
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        hide_during_completion = false,
+        debounce = 75,
+        keymap = {
+          accept = "<tab>",
+          next = "<C-l>",
+          prev = "<C-h>",
+          dismiss = "<C-c>",
+        },
+      },
+    },
   },
 }
